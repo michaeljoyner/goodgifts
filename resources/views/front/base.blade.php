b49282ce6f7b513e69aabd86c70cb40a7e0f85ee
Michael Joyner <joyner.michael@gmail.com>
2017-04-15 21:50:59 +0800

    update favicon

--- resources/views/front/base.blade.php
+++ resources/views/front/base.blade.php
@@ -14,6 +14,12 @@
             csrfToken: '{{ csrf_token() }}'
         }
     </script>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#2e2828">
     <script charset="UTF-8" src="//cdn.sendpulse.com/28edd3380a1c17cf65b137fe96516659/js/push/34627b6bccbae17bbc28edb20d8c659f_1.js" async></script>
 </head>
 <body class="@yield('bodyclass', 'scripted')">
