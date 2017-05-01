d2ae245a16ea87ad230825acca976ffa78a98c4a
Michael Joyner <joyner.michael@gmail.com>
2017-05-01 10:39:42 +0800

    let plain text exclude by class name

--- app/Http/Controllers/ArticleTextController.php
+++ app/Http/Controllers/ArticleTextController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Articles\Article;
 use Illuminate\Http\Request;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ArticleTextController extends Controller
 {
     public function show($slug)
     {
         $article = Article::where('slug', $slug)->first();
+
+        if(request('exclude', false)) {
+            $crawler = new Crawler();
+            $crawler->addHtmlContent($article->body);
+            $cards = $crawler->filter('.' . request('exclude'))->each(function($node) {
+                $node->getNode(0)->nodeValue = '';
+            });
+            return strip_tags($crawler->html());
+        }
+
         return strip_tags($article->body);
+
     }
 }
--- tests/Feature/Articles/PlainTextTest.php
+++ tests/Feature/Articles/PlainTextTest.php
@@ -35,4 +35,30 @@ class PlainTextTest extends TestCase
         $response->assertSee('This is the heading');
         $response->assertSee('This is the body.');
     }
+
+    /**
+     *@test
+     */
+    public function certain_elements_can_be_excluded_from_output()
+    {
+        $this->disableExceptionHandling();
+        $this->withoutMiddleware();
+        $html = '<h1>This is the heading</h1><p>This is the body.</p><div class="amzn-product-card">This is amazon stuff</div><img src="test.jpg" alt="foo">';
+        $article = factory(Article::class)->create(['body' => $html]);
+
+        $response = $this->get('/articles/' . $article->slug . '/plain-text?exclude=amzn-product-card');
+
+        $response->assertStatus(200);
+        $response->assertDontSee('<h1>');
+        $response->assertDontSee('</h1>');
+        $response->assertDontSee('<p>');
+        $response->assertDontSee('</p>');
+        $response->assertDontSee('<img');
+        $response->assertDontSee('test.jpg');
+        $response->assertDontSee('foo');
+        $response->assertDontSee('This is amazon stuff');
+        $response->assertDontSee('<div class="amzn-product-card">');
+        $response->assertSee('This is the heading');
+        $response->assertSee('This is the body.');
+    }
 }
