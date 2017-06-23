1a4e31cdece57e467ddbd20008d7769e3f7988f3
Michael Joyner <joyner.michael@gmail.com>
2017-06-24 02:21:55 +0800

    begin thanks page

--- resources/views/front/recommendations/thanks.blade.php
+++ resources/views/front/recommendations/thanks.blade.php
@@ -2,5 +2,12 @@
 
 @section('content')
     @include('front.partials.standardheader')
-    <h1>Shot, bru!</h1>
+    <h1>Thanks!</h1>
+    <p class="thanks-sub-title">That is one less thing for you to worry about.</p>
+    <p>Now it's up to us to get you a cracking list, and we aren't in the business of letting people down.</p>
+    <p>We'll be in touch.</p>
+    <div class="thaks-page-actions">
+        <a href="/">Back to the site</a>
+        <a href="/recommendations/signup">Add another guy</a>
+    </div>
 @endsection
