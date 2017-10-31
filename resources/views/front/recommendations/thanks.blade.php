f6e5f4826a27f6c55f3236e3d62b26600767aca9
Michael Joyner <joyner.michael@gmail.com>
2017-10-31 14:29:09 +0800

    thank you page tachyons

--- resources/views/front/recommendations/thanks.blade.php
+++ resources/views/front/recommendations/thanks.blade.php
@@ -1,15 +1,15 @@
 @extends('front.base')
 
 @section('content')
-    @include('front.partials.standardheader')
-    <div class="thanks-container">
-        <h1>Nicely done.</h1>
-        <p class="thanks-sub-title">That's one less thing for you to worry about.</p>
+    @include('front.partials.navbar')
+    <div class="measure center-ns mh4">
+        <h1 class="f3 f1-ns strong-font">Nicely done.</h1>
+        <p class="f4">That's one less thing for you to worry about.</p>
         <p>Now it's up to us to get you a cracking list, and we aren't in the business of letting people down.</p>
         <p>We'll be in touch.</p>
-        <div class="thaks-page-actions">
-            <a class="action-button" href="/">Back to the site</a>
-            <a class="action-button" href="/recommendations/signup">Add another guy</a>
+        <div class="">
+            <a class="inline-flex mt4 items-center ba bw2 pa2 strong-font ttu mr3 link col-d hov-p" href="/">Back to the site</a>
+            <a class="inline-flex mt4 items-center ba bw2 pa2 strong-font ttu mr3 link col-d hov-p" href="/recommendations/signup">Add another guy</a>
         </div>
         {{--<img class="logo" src="/images/logos/ggfg_symbol_red.png" alt="">--}}
     </div>
