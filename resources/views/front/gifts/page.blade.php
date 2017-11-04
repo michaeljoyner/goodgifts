ca1f843b8de9711829ddf5f4d5fb0e5df312a5ce
Michael Joyner <joyner.michael@gmail.com>
2017-11-04 10:26:29 +0800

    better messaging in gifts app

--- resources/views/front/gifts/page.blade.php
+++ resources/views/front/gifts/page.blade.php
@@ -4,7 +4,7 @@
     @include('front.partials.navbar')
     <header>
         <h1 class="f3 f1-ns strong-font tc">Find a Gift</h1>
-        <p class="f5 f4-ns tc col-gr measure-wide lh-copy center-ns mh3">We mix your budget with his interests to serve up the best gift ideas.</p>
+        <p class="f5 tc col-gr measure-wide lh-copy center-ns mh3">We mix your budget with his interests to serve up the best gift ideas.</p>
     </header>
     <gifts-app></gifts-app>
 
