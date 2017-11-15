ca3d87fc573d09b1c2627c0dc7cdb4c48b62975d
Michael Joyner <joyner.michael@gmail.com>
2017-11-15 09:11:03 +0800

    remove old signup conversion

--- resources/views/front/recommendations/signup.blade.php
+++ resources/views/front/recommendations/signup.blade.php
@@ -198,25 +198,3 @@
     </section>
 
 @endsection
-
-@section('bodyscripts')
-    <script type="text/javascript">
-        /* <![CDATA[ */
-        var google_conversion_id = 858669646;
-        var google_conversion_language = "en";
-        var google_conversion_format = "3";
-        var google_conversion_color = "ffffff";
-        var google_conversion_label = "QkQxCN7O63QQzoS5mQM";
-        var google_remarketing_only = false;
-        /* ]]> */
-    </script>
-    <script type="text/javascript"
-            src="//www.googleadservices.com/pagead/conversion.js">
-    </script>
-    <noscript>
-        <div style="display:inline;">
-            <img height="1" width="1" style="border-style:none;" alt=""
-                 src="//www.googleadservices.com/pagead/conversion/858669646/?label=QkQxCN7O63QQzoS5mQM&amp;guid=ON&amp;script=0"/>
-        </div>
-    </noscript>
-@endsection
