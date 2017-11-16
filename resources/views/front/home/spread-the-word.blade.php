967d911ec8c3c3d2b6e70765863563e229653942
Michael Joyner <joyner.michael@gmail.com>
2017-11-16 13:05:33 +0800

    correct typo and add social links

--- resources/views/front/home/spread-the-word.blade.php
+++ resources/views/front/home/spread-the-word.blade.php
@@ -1,8 +1,8 @@
 <section class="pv5">
-    <p class="f3 f2-ns tc mt0 strong-font">Spread the word. Share the love</p>
-    <p class="tc measure-wide-ns mh3 center-ns">A little click goes a long way, so help a couple of brothers out. Like, share or tell a friend. We'll never stop loving you if you do. Thank's ya'll.</p>
+    <p class="f3 f2-ns tc mt0 strong-font">Spread the word. Share the love.</p>
+    <p class="tc measure-wide-ns mh3 center-ns">A little click goes a long way, so help a couple of brothers out. Like, share or tell a friend. We'll never stop loving you if you do. Thanks ya'll.</p>
     <div class="flex items-center justify-center mt4">
-        <a class="col-d hov-p mh2 di" href="#">@include('svgicons.social.facebook-f')</a>
-        <a class="col-d hov-p mh2 di" href="#">@include('svgicons.social.instagram')</a>
+        <a class="col-d hov-p mh2 di" href="https://www.facebook.com/goodgiftsforguys/">@include('svgicons.social.facebook-f')</a>
+        <a class="col-d hov-p mh2 di" href="https://www.instagram.com/good_gifts_for_guys/">@include('svgicons.social.instagram')</a>
     </div>
 </section>
